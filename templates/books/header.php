16c03e3849cc52683ab0e0e67796d01907d41e41
edFN <38671587+edFN@users.noreply.github.com>
2022-07-08 11:35:12 +0300

    Add files via upload

---
+++ templates/books/header.php
@@ -0,0 +1,13 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<?$APPLICATION->ShowHead()?>
+<title><?$APPLICATION->ShowTitle()?></title>
+</head>
+
+
+<body>
+
+      
